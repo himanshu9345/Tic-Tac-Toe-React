dae5bfe60ba83b51b2560ee6a16674a6f2b1b1cb
himanshu9345 <hbp53@njit.edu>
2020-06-22 01:01:53 -0400

    added states, props to square component

--- board-game/src/components/Square.js
+++ board-game/src/components/Square.js
@@ -1,8 +1,25 @@
 import React from "react";
+/*
+Components : subclasses , reusable code
+Props (properties) : pass data from parent to child class
+states : each component can have state to remember things
 
+*/
 class Square extends React.Component {
+  constructor(props) {
+    // Super in all reach subclass
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 export default Square;
